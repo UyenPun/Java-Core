0963620cef924fa25ec428e180bee19ae01acc67
UyenPun <tranuyen2018@gmail.com>
2024-09-30 00:02:39 +0700

    41:46 Variables (Khái niệm biến).

--- src/javacore/HelloWorld.java
+++ src/javacore/HelloWorld.java
@@ -2,6 +2,12 @@ package javacore;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		int a = 6;
+		int b = 9;
+		int c = a + b;
+		System.out.println("Sum: " + c);
+
+		int test1 = 5 + 9 * 3 - 10 / 2; // Nhân chia trước cộng trừ sau
+		System.out.println("test1: " + test1);
 	}
 }
