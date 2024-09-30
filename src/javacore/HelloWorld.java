28ad275a690745a299969107cbae99a31baf99e3
UyenPun <tranuyen2018@gmail.com>
2024-09-30 09:25:28 +0700

    01:52:43 Java If...Else.

--- src/javacore/HelloWorld.java
+++ src/javacore/HelloWorld.java
@@ -4,27 +4,43 @@ import java.util.Scanner;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		// Min or Max
-		int a = 5;
-		int b = 6;
-		int c = Math.max(a, b);
-		System.out.println("Max = " + c);
+		int score = 10;
 
-		// Lũy thừa
-		double d = 2;
-		double e = 2;
-		double f = Math.pow(d, e);
-		System.out.println("Pow = " + f);
+		// Neu True moi in ra
+		if (score > 5) {
+			System.out.println("Qua mon");
+		}
+		// Nếu dk trên không xảy ra thì dk phủ định của nó là else
+		else {
+			System.out.println("Rot mon");
 
-		// Thực hành
+		}
+
+		// System.out.println("Run here: " + (score == 5));
+
+		System.out.println("============================= Bai Tap =============================");
+
+		// Viết chương trình cho phép nhập vào số tiền thuế (đơn vị là triệu đồng), và
+		// in ra kết quả:
+		// - dưới 10M: không đóng thuế
+		// - từ 10M tới 15M: thuế 10%
+		// - từ 15 tới 30M : thuế 20%
+		// - trên 30M: thuế 50%
+		// Ví dụ: nhập vào 25M -> in ra kết quả "thuế 20%
+		// tax: tien thue
+
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("First number: ");
-		int num1 = scanner.nextInt();
+		System.out.print("Nhap so tax: ");
+		double tax = scanner.nextDouble();
 
-		System.out.print("Second number: ");
-		int num2 = scanner.nextInt();
-		System.out.println("Min = " + Math.min(num1, num2));
-		scanner.close();
-
+		if (tax < 10_000_000) {
+			System.out.println("Khong dong thue");
+		} else if (tax >= 10_000_000 && tax < 15_000_000) {
+			System.out.println("Thue 10%");
+		} else if (tax >= 15_000_000 && tax < 30_000_000) {
+			System.out.println("Thue 20%");
+		} else if (tax >= 30_000_000) {
+			System.out.println("Thue 50%");
+		}
 	}
 }
