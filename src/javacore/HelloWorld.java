b692ff5202be250094c720e687716ad410e6cc2d
UyenPun <tranuyen2018@gmail.com>
2024-09-30 16:29:46 +0700

    03:20:46 Luyện tập Array và Loop - Bai tap sort.

--- src/javacore/HelloWorld.java
+++ src/javacore/HelloWorld.java
@@ -6,27 +6,22 @@ import java.util.Scanner;
 public class HelloWorld {
 	public static void main(String[] args) {
 		// CÁCH 1:
-		String[] cars = { "Volvo", "BMW", "Ford", "Mazda" }; // 0 1 2 (Không thể thêm phân tử or xóa đi)
+		int[] numbers = { 1, 20, 5, 8, 9, 10, 2 };
 
-		System.out.println("Length: " + cars.length); // độ dài
-		System.out.println("Phan tu: " + cars[0]); // phần tử
-		System.out.println(Arrays.toString(cars)); // in ra mảng
+		Arrays.sort(numbers); // sắp xếp
 
-		// Loop Through an Array (duyệt tất cả phần tử của mảng)
-		for (int i = 0; i < cars.length; i++) {
-			System.out.println(cars[i]);
+		System.out.println(Arrays.toString(numbers));
+
+		// Cách sắp xếp sort bằng for
+		for (int i = 0; i < numbers.length - 1; i++) {
+			for (int j = i + 1; j < numbers.length; j++) {
+				if (numbers[i] > numbers[j]) { // <: giảm dần
+					int temp = numbers[i];
+					numbers[i] = numbers[j];
+					numbers[j] = temp;
+				}
+			}
 		}
-
-		// ForEach:
-		for (String i : cars) {
-			System.out.println(i);
-		}
-
-		// CÁCH 2:
-		String[] car = new String[5]; // khai báo mảng có 5 phần tử
-		car[0] = "1";
-
-		System.out.println(car[0]);
-
+		System.out.println(Arrays.toString(numbers));
 	}
 }
