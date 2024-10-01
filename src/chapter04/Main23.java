7c33864742706e492de42544de7abb7aafde0334
UyenPun <tranuyen2018@gmail.com>
2024-10-01 10:12:37 +0700

    04:22:02 Class Method (Phương thức của class).

--- src/chapter04/Main23.java
+++ src/chapter04/Main23.java
@@ -1,11 +1,19 @@
 package chapter04;
 
 public class Main23 {
+  int sum(int a, int b) {
+    return a + b;
+  }
+
   public static void main(String[] args) {
     Student student1 = new Student(); // Khởi báo obj và gán giá trị
     student1.name = "Uyen";
     student1.age = 22;
 
     System.out.println(student1.name + " " + student1.age);
+
+    // Su dung method
+    Main23 testMain23 = new Main23();
+    System.out.println(testMain23.sum(4, 5));
   }
 }
--- src/chapter04/Student.java
+++ src/chapter04/Student.java
@@ -1,10 +1,24 @@
 package chapter04;
 
 public class Student {
+  // Class Attributes
   String name;
   int age;
 
-  public void learnJava() {
-    System.out.println("Learn Java with Youtube Hỏi Dân IT");
+  // Class Method
+  void learnJava() {
+    System.out.println("Learn Java with Youtube");
   }
+
+  // ================================================================
+
+  // <kiểu_trả_về> <tên_method> ([danh_sách_tham_số]) { //code}
+  // ví dụ:
+
+  // void getName() {
+  // }
+
+  // int getTuoi() {
+  // }
+  // Với void=>return"nothing"
 }
