84c8bbf017c91d417f9fb3c3d032cfa566ed70dc
UyenPun <tranuyen2018@gmail.com>
2024-10-02 15:52:04 +0700

    07:24:49 Java Inheritance.

---
+++ src/Inheritance/Vehicle.java
@@ -0,0 +1,9 @@
+package Inheritance;
+
+public class Vehicle {
+  protected String brand = "Ford"; // Vehicle attribute
+
+  public void honk() { // Vehicle method
+    System.out.println("Tuut, tuut!");
+  }
+}
